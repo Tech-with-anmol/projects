4e2734b225c9896ecc9e1f729f8ca1a5d986328e
PGamerX <websitedesigner123456@gmail.com>
2021-05-13 10:35:28 +0530

    Create pip.py

---
+++ examples/python/ai-responses/Version 3/pip.py
@@ -0,0 +1,19 @@
+import aiohttp
+import asyncio
+import urllib.parse
+message = "Hello!"
+encoded = urllib.parse.quote(message)
+url = "https://api.pgamerx.com/v3/ai/response/"
+key = "Your API Key"
+header = {"x-api-key": key}
+type = "stable"
+params = {'type':type , 'message':encoded}
+async def start():
+    async with aiohttp.ClientSession(headers=header) as session:
+        async with session.get(url='https://api.pgamerx.com/v3/ai/response', params=params) as resp:
+            text = await resp.json()
+            print(resp.status)
+            print(text[0]['message'])
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start())
